48f4bfc21e89bdce6b47610bf5afa88f24cba641
GarvBakliwal <garvbakliwal04@gmail.com>
2025-06-09 23:48:23 +0530

    Remove commented-out fields from transactionModel and userModel for cleaner code

--- src/models/transactionModel.ts
+++ src/models/transactionModel.ts
@@ -5,30 +5,13 @@ const transactionSchema = new mongoose.Schema({
   accountId: { type: String, required: true },
   transactionId: { type: String, required: true, unique: true },
   name: String,
-  // merchant_name: String,
-  // website: String,
   amount: Number,
-  // iso_currency_code: String,
   date: String,
-  // authorized_date: String,
-  // category: [String],
   payment_channel: String,
   pending: Boolean,
-  // logo_url: String,
   category: {
     type: String
   },
-  //   counterparties: [ {type : Object}
-  //   // {
-  //   //   confidence_level: { type: String },
-  //   //   entity_id: { type: String },
-  //   //   logo_url: { type: String },
-  //   //   name: { type: String },
-  //   //   phone_number: { type: String },
-  //   //   type: { type: String },
-  //   //   website: { type: String },
-  //   // }
-  // ]
 }, { timestamps: true });
 
 export default mongoose.model('Transaction', transactionSchema);
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,4 +1,3 @@
-import { NextFunction } from "express";
 import mongoose, { Schema } from "mongoose"
 import bcrypt from 'bcryptjs'
 
@@ -6,38 +5,16 @@ const userSchema: Schema = new mongoose.Schema({
     firstName: {
         type: String,
         required: [true, "Name is Required"],
-        // length: [20, "Name Cannot exceed 20 Characters"]
     },
     lastName: {
         type: String,
         required: [true, "Name is Required"],
-        // length: [20, "Name Cannot exceed 20 Characters"]
     },
     email: {
         type: String,
         required: [true, "Email is Required"],
         unique: true
     },
-    // address: {
-    //     type: String,
-    //     required: [true,"Address is Required"],
-    // },
-    // city: {
-    //     type: String,
-    //     required: [true,"City is Required"]
-    // },
-    // state: {
-    //     type: String,
-    //     required: [true,"State is Required"]
-    // },
-    // dateOfBirth: {
-    //     type: String,
-    //     required: [true,"Date of Birth is Required"]
-    // },
-    // postalCode : {
-    //     type: String,
-    //     required:[true,"Postal Code is Required"]
-    // },
     password: {
         type: String,
         required: true
