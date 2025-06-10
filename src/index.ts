e877b1abf4308ac5770d66e3e8402985757b9a03
GarvBakliwal <garvbakliwal04@gmail.com>
2025-06-10 13:36:54 +0530

    Refactor CORS configuration to include dynamic site URL

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,14 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', `${process.env.SITE_URL}`],
     credentials: true
-}))
+}));
 app.use(express.json());
-app.get('/',(req : Request,res: Response)=>{
+app.get('/', (req: Request, res: Response) => {
     res.send('HOMEPAGE')
 });
-app.use('/api',routes);
+app.use('/api', routes);
 connectDB();
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
