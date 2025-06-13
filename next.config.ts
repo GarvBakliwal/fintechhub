0f30295ede40919a7a8a3a13e5b98554ac1eaa71
GarvBakliwal <garvbakliwal04@gmail.com>
2025-06-13 18:16:00 +0530

    chore: update Next.js to version 15.3.3 and remove port specification in start script; add sharp dependency

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
   },
 };
 
