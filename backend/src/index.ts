26fd7346adefb765d84951b4398212df34a42970
GarvBakliwal <garvbakliwal04@gmail.com>
2026-02-21 19:49:38 +0530

    Add API documentation and static file serving
    - Created an HTML file for API documentation with structured sections for Auth, Accounts, Transactions, Transfers, and Plaid.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import { connectDB } from './lib/dbConnection';
 import routes from './routes/index'
 import cors from 'cors'
 import helmet from 'helmet';
+import path from 'path';
 dotenv.config();
 
 const app = express();
@@ -11,12 +12,24 @@ app.use(cors({
     origin: ['http://localhost:3000', `${process.env.SITE_URL}`,`${process.env.SITE_URL}:3000`,'https://fintechhub.site','http://fintechhub.site'],
     credentials: true
 }));
-app.use(helmet());
+
+
+app.use(
+    '/',
+    helmet({
+        contentSecurityPolicy: false,
+    })
+);
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(express.json());
-app.get('/', (req: Request, res: Response) => {
-    res.send('HOMEPAGE')
-});
+
+app.use('/api', helmet());
 app.use('/api', routes);
+
+app.get('/docs', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "public"));
+});
 connectDB();
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
